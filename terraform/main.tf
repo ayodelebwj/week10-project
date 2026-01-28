d12925b61648bb22d9d0b960490e0beb687402a4
olaniyi <ayodelebwj@gmail.com>
2026-01-28 01:27:52 -0500

    push15

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,6 @@ terraform {
     region  = "us-east-2"
     encrypt = true
   }
-
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "= 5.0.0"
-    }
-  }
 }
 
 
