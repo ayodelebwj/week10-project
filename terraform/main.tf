fe7283c1a179ad629225402f90bb9dc2c1700436
olaniyi <ayodelebwj@gmail.com>
2026-01-28 01:37:03 -0500

    ppp

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,15 @@ terraform {
     region  = "us-east-2"
     encrypt = true
   }
+
+  //required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0.0"
+    }
+  }
 }
 
 
