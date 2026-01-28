91b7896ce94b81576c2d64c387a961d964a6a1eb
olaniyi <ayodelebwj@gmail.com>
2026-01-28 01:16:07 -0500

    push9

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,21 @@
 terraform {
-    backend "s3" {
-        bucket = "techbleatweek8"
-        key = "env/prod/terraform.statefile"
-        region = "us-east-2"
-        encrypt = true
-    }
+  backend "s3" {
+    bucket  = "techbleatweek8"
+    key     = "env/prod/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
 
-    required_version = ">= 1.5.0"
+  required_version = ">= 1.5.0"
 
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-                }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
+
 
 provider "aws" {
   region = "us-east-2"
